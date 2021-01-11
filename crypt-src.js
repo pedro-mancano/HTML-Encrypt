518e0b30bfd3feff368b26b9c6d893f3f15b6ae8
Pedro Mançano <pedro-mancano@users.noreply.github.com>
2021-01-11 17:23:03 -0300

    fix

---
+++ crypt-src.js
@@ -0,0 +1,103 @@
+!function crypt(){
+    if(this == window){
+        return new crypt();
+    }
+    
+    this.page = "";
+
+    this.params = () => {
+        for(var i of document.getElementsByTagName('script')[0].src.split('?')[1].split('&')){
+            var b;
+            if(i && (b=i.split('=')).length == 2){
+                if(b[0]=='v'){
+                    this.page = "./"+b[1];
+                }
+            }
+        }
+    }
+    
+    this.clear = () => {
+        for(var v of document.children){
+            v.remove();
+        }
+    };
+
+    this.jsonToElement = (value, parent) => {
+        if(Array.isArray(value)){
+            for(var v in value){
+                this.jsonToElement(value[v], parent);
+            }
+        }else{
+            var el = document.createElement(value.tag);
+            el.async = false;
+            for(var attrs of value.attributes){
+                el.setAttribute(attrs.name, attrs.value);
+            }
+            if(value.text){
+                el.innerText = value.text;
+            }
+            for(var child of value.children){
+                this.jsonToElement(child, el);
+            }
+            if(parent){
+                parent.append(el);
+            }else{
+                document.childNodes[1].append(el);
+            }
+        }
+    };
+
+    this.elementToJson = (el, ignoreThis) => {
+        var text = this.getText(el);
+        var children = [];
+        for(var child of el.children){
+            children.push(this.elementToJson(child));
+        }
+        var value = {
+            tag:el.localName,
+            attributes:this.NamedNodeMapToArray(el.attributes),
+            text:text,
+            children:children
+        };
+        return ignoreThis ? children : value;
+       }
+    
+    this.NamedNodeMapToArray = (nnm) => {
+        var b = [];
+        for(var n of nnm){
+            b.push({name:n.localName,value:n.nodeValue});
+        }
+        return b;
+    }
+    
+    this.getText = (el) => {
+        var str = "";
+        var children = el.childNodes;
+        for(var child in children){
+            if(children[child].nodeName == "#text" && children[child].nodeValue){
+                str += children[child].nodeValue.trim();
+            }
+        }
+        return str;
+    }
+
+    this.createPage = (json) => {
+        this.clear();
+        this.jsonToElement(json, document);
+    }
+
+    this.request = () => {
+        this.params();
+        var cryptInstance = this;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                cryptInstance.createPage(JSON.parse(xhttp.responseText));
+            }
+        };
+        xhttp.open("GET", this.page, true);
+        xhttp.send();
+    }
+}().request();
+
+
