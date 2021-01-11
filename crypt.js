eb6c5bdeb0e4375e8eab9a5ae9379f4559b3be67
Pedro Mançano <pedro-mancano@users.noreply.github.com>
2021-01-11 16:56:39 -0300

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -90,7 +90,7 @@
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                this.createPage(xhttp.responseText);;
+                crypt.createPage(xhttp.responseText);;
             }
         };
         xhttp.open("GET", this.page, true);
